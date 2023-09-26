081de092532284f04cef0ee64e43f40434fe52d6
Harun Yiğit <harun.ygt03@gmail.com>
2023-09-26 14:51:28 +0300

    CableMania

---
+++ CableMania/Assets/Scripts/Socket.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Socket : MonoBehaviour
+{
+    [SerializeField] private bool Fullness;
+    [SerializeField] private GameObject MovementPos;
+    
+}
